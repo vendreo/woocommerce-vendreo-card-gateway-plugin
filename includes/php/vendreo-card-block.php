6cd09f992d3e09612a0f8683b7b5726ac271578b
Dean Haines <vbpupil@gmail.com>
2024-01-12 10:43:14 +0000

    new linter added

--- includes/php/vendreo-card-block.php
+++ includes/php/vendreo-card-block.php
@@ -2,51 +2,47 @@
 
 use Automattic\WooCommerce\Blocks\Payments\Integrations\AbstractPaymentMethodType;
 
-final class Vendreo_Card_Gateway_Blocks extends AbstractPaymentMethodType
-{
-    private $gateway;
+final class Vendreo_Card_Gateway_Blocks extends AbstractPaymentMethodType {
 
-    protected $name = 'woocommerce_vendreo_card_gateway';
+	private $gateway;
 
-    public function initialize()
-    {
-        $this->settings = get_option('woocommerce_woocommerce_vendreo_card_gateway_settings', []);
-        $this->gateway = new WooCommerce_Vendreo_Card_Gateway();
-    }
+	protected $name = 'woocommerce_vendreo_card_gateway';
 
-    public function is_active()
-    {
-        return $this->gateway->is_available();
-    }
+	public function initialize() {
+		$this->settings = get_option( 'woocommerce_woocommerce_vendreo_card_gateway_settings', [] );
+		$this->gateway  = new WooCommerce_Vendreo_Card_Gateway();
+	}
 
-    public function get_payment_method_script_handles()
-    {
-        wp_register_script(
-            'woocommerce_vendreo_card_gateway-blocks-integration',
-            VENDREO_CARD__PLUGIN_DIR_PATH . '/includes/js/vendreo-card-checkout.js',
-            [
-                'wc-blocks-registry',
-                'wc-settings',
-                'wp-element',
-                'wp-html-entities',
-                'wp-i18n',
-            ],
-            null,
-            true
-        );
+	public function is_active() {
+		return $this->gateway->is_available();
+	}
 
-        if (function_exists('wp_set_script_translations')) {
-            wp_set_script_translations('woocommerce_vendreo_card_gateway-blocks-integration');
-        }
+	public function get_payment_method_script_handles() {
+		wp_register_script(
+			'woocommerce_vendreo_card_gateway-blocks-integration',
+			VENDREO_CARD__PLUGIN_DIR_PATH . '/includes/js/vendreo-card-checkout.js',
+			[
+				'wc-blocks-registry',
+				'wc-settings',
+				'wp-element',
+				'wp-html-entities',
+				'wp-i18n',
+			],
+			null,
+			true
+		);
 
-        return ['woocommerce_vendreo_card_gateway-blocks-integration'];
-    }
+		if ( function_exists( 'wp_set_script_translations' ) ) {
+			wp_set_script_translations( 'woocommerce_vendreo_card_gateway-blocks-integration' );
+		}
 
-    public function get_payment_method_data()
-    {
-        return [
-            'title' => $this->gateway->title,
-            'description' => $this->gateway->description,
-        ];
-    }
+		return [ 'woocommerce_vendreo_card_gateway-blocks-integration' ];
+	}
+
+	public function get_payment_method_data() {
+		return [
+			'title'       => $this->gateway->title,
+			'description' => $this->gateway->description,
+		];
+	}
 }
