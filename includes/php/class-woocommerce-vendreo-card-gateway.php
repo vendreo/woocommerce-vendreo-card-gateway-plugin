b7932e7224f4aa077c9dbb1c641e5d4be9a3515f
Dean Haines <vbpupil@gmail.com>
2024-01-15 10:09:35 +0000

    spaces swapped for tabs

--- includes/php/class-woocommerce-vendreo-card-gateway.php
+++ includes/php/class-woocommerce-vendreo-card-gateway.php
@@ -104,7 +104,7 @@ class WooCommerce_Vendreo_Card_Gateway extends WC_Payment_Gateway {
 
 		$response = wp_remote_post(
             $this->url,
-            array(
+            [
                 'method' => 'POST',
                 'headers'     => [
                     'Content-Type' => 'application/json',
@@ -115,7 +115,7 @@ class WooCommerce_Vendreo_Card_Gateway extends WC_Payment_Gateway {
                 'httpversion' => '1.0',
                 'timeout' => 45,
                 'body' => json_encode( $post ),
-            )
+            ]
         );
 
         if ( is_wp_error( $response ) ) {
